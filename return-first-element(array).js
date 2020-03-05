503c99ec98fc4e43f668ad67eea58c4e039c119c
Oleg Chepil <west0ne@MacBook-Pro-Oleg.local>
2020-03-05 23:55:32 +0200

    add files

--- return-first-element(array).js
+++ return-first-element(array).js
@@ -1,8 +1,11 @@
 /* 
 Return the First Element in an Array
 */
+//SOLUTION:
+
 function getFirstValue(arr) {
     return arr[0];	
 }
 let result = getFirstValue([3, 4, 77, 23])
 console.log(result);
+// result = 3;
